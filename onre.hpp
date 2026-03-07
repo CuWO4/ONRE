6e55f944ba77c851bb82423d506830a61d063558
CuWO4 <wutong.tony@outlook.com>
2026-03-07 22:38:53 +0800

    feat: use GNU extension `string literal operator templates` to simplify interface

--- onre.hpp
+++ onre.hpp
@@ -2091,217 +2091,257 @@ struct BuildMutualTable<NrGroups, TypeList<MutualPairs...>> {
 } /* namespace impl */
 
 /* === interface === */
-template<impl::FixedString Pattern>
-inline bool match(std::string_view str) noexcept;
-template<impl::FixedString Pattern>
-inline std::string replace(std::string_view rule, std::string_view str) noexcept;
+#define USE_ONRE \
+using onre::operator""_match; \
+using onre::operator""_replace
 
 template<impl::FixedString Pattern>
-inline bool match(std::string_view str) noexcept {
-  using Re = typename impl::RegexScan<Pattern>::type;
-  using NoActionRe = typename impl::dfa::RemoveAllAction<Re>::type;
-  using DFAStatesList = impl::dfa::AllStatesList<NoActionRe>;
-  using DFAEdgesList  = impl::dfa::AllEdgesList<NoActionRe>;
-  static constexpr std::size_t nr_dfa_states = DFAStatesList::length;
-  static constexpr auto dfa_trans_table
-    = impl::dfa::BuildTable<nr_dfa_states, DFAEdgesList>::make();
-  static constexpr auto dfa_is_accept_states = impl::dfa::BuildAccepts<DFAStatesList>::make();
+class Matcher {
+public:
+  constexpr Matcher() {}
+  bool operator()(std::string_view str) noexcept {
+    using Re = typename impl::RegexScan<Pattern>::type;
+    using NoActionRe = typename impl::dfa::RemoveAllAction<Re>::type;
+    using DFAStatesList = impl::dfa::AllStatesList<NoActionRe>;
+    using DFAEdgesList  = impl::dfa::AllEdgesList<NoActionRe>;
+    static constexpr std::size_t nr_dfa_states = DFAStatesList::length;
+    static constexpr auto dfa_trans_table
+      = impl::dfa::BuildTable<nr_dfa_states, DFAEdgesList>::make();
+    static constexpr auto dfa_is_accept_states = impl::dfa::BuildAccepts<DFAStatesList>::make();
 
-  std::size_t state = 0;
-  for (const char& ch : str) {
-    if (ch < 0) [[unlikely]] return false;
-    int32_t nxt = dfa_trans_table[state][static_cast<std::size_t>(ch)];
-    if (nxt < 0) return false;
-    state = static_cast<std::size_t>(nxt);
+    std::size_t state = 0;
+    for (const char& ch : str) {
+      if (ch < 0) [[unlikely]] return false;
+      int32_t nxt = dfa_trans_table[state][static_cast<std::size_t>(ch)];
+      if (nxt < 0) return false;
+      state = static_cast<std::size_t>(nxt);
+    }
+    return dfa_is_accept_states[state];
   }
-  return dfa_is_accept_states[state];
-}
+};
 
 template<impl::FixedString Pattern>
-inline std::string replace(std::string_view replace_rule, std::string_view str) noexcept {
-  using Re = typename impl::RegexScan<Pattern>::type;
-  using StateList = impl::tnfa::AllStatesList<Re>;
-  using EdgeList  = impl::tnfa::AllEdgesList<Re>;
-  static constexpr std::size_t nr_states = StateList::length;
-  static constexpr std::size_t nr_used_slots = impl::tnfa::NrUsedSlots<Re>::value;
-  static constexpr std::size_t max_trans_action_length
-    = impl::tnfa::MaxTransActionLength<nr_states, EdgeList>::value;
-  static constexpr std::size_t max_accept_action_length
-    = impl::tnfa::MaxAcceptActionLength<StateList>::value;
-  static constexpr std::size_t nr_capture_group = nr_used_slots / 2;
+class Replacer {
+public:
+  constexpr Replacer() {}
+  std::string operator()(std::string_view replace_rule, std::string_view str) noexcept {
+    using Re = typename impl::RegexScan<Pattern>::type;
+    using StateList = impl::tnfa::AllStatesList<Re>;
+    using EdgeList  = impl::tnfa::AllEdgesList<Re>;
+    static constexpr std::size_t nr_states = StateList::length;
+    static constexpr std::size_t nr_used_slots = impl::tnfa::NrUsedSlots<Re>::value;
+    static constexpr std::size_t max_trans_action_length
+      = impl::tnfa::MaxTransActionLength<nr_states, EdgeList>::value;
+    static constexpr std::size_t max_accept_action_length
+      = impl::tnfa::MaxAcceptActionLength<StateList>::value;
+    static constexpr std::size_t nr_capture_group = nr_used_slots / 2;
 
-  /* S x Alphabet -> int32_t[], -1 represent no trans */
-  static constexpr auto trans_table = impl::tnfa::BuildTransTable<nr_states, EdgeList>::make();
-  /* S -> bool */
-  static constexpr auto accept_table = impl::tnfa::BuildAcceptTable<StateList>::make();
-  /* S x Alphabet x S -> int32_t[], -1 represent no action */
-  static constexpr auto trans_action_table
-    = impl::tnfa::BuildTransActionTable<nr_states, max_trans_action_length, EdgeList>::make();
-  /* S -> int32_t[], -1 represent no action */
-  static constexpr auto accept_action_table
-    = impl::tnfa::BuildAcceptActionTable<max_accept_action_length, StateList>::make();
+    /* S x Alphabet -> int32_t[], -1 represent no trans */
+    static constexpr auto trans_table = impl::tnfa::BuildTransTable<nr_states, EdgeList>::make();
+    /* S -> bool */
+    static constexpr auto accept_table = impl::tnfa::BuildAcceptTable<StateList>::make();
+    /* S x Alphabet x S -> int32_t[], -1 represent no action */
+    static constexpr auto trans_action_table
+      = impl::tnfa::BuildTransActionTable<nr_states, max_trans_action_length, EdgeList>::make();
+    /* S -> int32_t[], -1 represent no action */
+    static constexpr auto accept_action_table
+      = impl::tnfa::BuildAcceptActionTable<max_accept_action_length, StateList>::make();
 
-  using SlotLine = std::array<int32_t, nr_used_slots>;
-  using SlotFile = std::array<SlotLine, nr_states>;
+    using SlotLine = std::array<int32_t, nr_used_slots>;
+    using SlotFile = std::array<SlotLine, nr_states>;
 
-  static auto open_time = [](const SlotLine& line, size_t group_idx) {
-    return line[group_idx << 1];
-  };
-  static auto close_time = [](const SlotLine& line, size_t group_idx) {
-    return line[group_idx << 1 | 1];
-  };
-  static auto is_opened = [](const SlotLine& line, size_t group_idx) {
-    return open_time(line, group_idx) >= 0;
-  };
-  static auto is_closed = [](const SlotLine& line, size_t group_idx) {
-    return close_time(line, group_idx) >= 0;
-  };
-  static auto group_len = [](const SlotLine& line, size_t group_idx) {
-    return close_time(line, group_idx) - open_time(line, group_idx);
-  };
+    static auto open_time = [](const SlotLine& line, size_t group_idx) {
+      return line[group_idx << 1];
+    };
+    static auto close_time = [](const SlotLine& line, size_t group_idx) {
+      return line[group_idx << 1 | 1];
+    };
+    static auto is_opened = [](const SlotLine& line, size_t group_idx) {
+      return open_time(line, group_idx) >= 0;
+    };
+    static auto is_closed = [](const SlotLine& line, size_t group_idx) {
+      return close_time(line, group_idx) >= 0;
+    };
+    static auto group_len = [](const SlotLine& line, size_t group_idx) {
+      return close_time(line, group_idx) - open_time(line, group_idx);
+    };
 
-  static auto is_digit = [](char ch) { return '0' <= ch && ch <= '9'; };
+    static auto is_digit = [](char ch) { return '0' <= ch && ch <= '9'; };
 
-  static auto apply_action = []<size_t N>(
-    const SlotLine& old_line,
-    const std::array<int32_t, N>& actions,
-    int32_t p
-  ) {
-    SlotLine new_line{old_line};
-    for (const auto& action : actions) {
-      if (action < 0) break;
-      new_line[action] = p;
-    }
-    return new_line;
-  };
-
-  // heuristically choose a slot configuration to try to get longest match
-  static auto need_change = [](const SlotLine& old_line, const SlotLine& new_line) {
-    for (size_t k = 0; k < nr_capture_group; k++) {
-      if (!is_opened(old_line, k) && !is_opened(new_line, k)) continue;
-      if (is_opened(old_line, k) && !is_opened(new_line, k)) return false;
-      if (!is_opened(old_line, k) && is_opened(new_line, k)) return true;
-      if (!is_closed(old_line, k) && !is_closed(new_line, k)) {
-        if (open_time(old_line, k) < open_time(new_line, k)) return false;
-        if (open_time(old_line, k) > open_time(new_line, k)) return true;
-        continue;
+    static auto apply_action = []<size_t N>(
+      const SlotLine& old_line,
+      const std::array<int32_t, N>& actions,
+      int32_t p
+    ) {
+      SlotLine new_line{old_line};
+      for (const auto& action : actions) {
+        if (action < 0) break;
+        new_line[action] = p;
       }
-      if (!is_closed(old_line, k) && is_closed(new_line, k)) return false;
-      if (is_closed(old_line, k) && !is_closed(new_line, k)) return true;
-      if (group_len(old_line, k) > group_len(new_line, k)) return false;
-      if (group_len(old_line, k) < group_len(new_line, k)) return true;
-      if (open_time(old_line, k) > open_time(new_line, k)) return false;
-      if (open_time(old_line, k) < open_time(new_line, k)) return true;
-    }
-    return false;
-  };
+      return new_line;
+    };
 
-  thread_local static SlotFile slot_file1, slot_file2;
-  thread_local static std::array<bool, nr_states> is_state_active1, is_state_active2;
-  thread_local static std::vector<size_t> active_states1, active_states2;
+    // heuristically choose a slot configuration to try to get longest match
+    static auto need_change = [](const SlotLine& old_line, const SlotLine& new_line) {
+      for (size_t k = 0; k < nr_capture_group; k++) {
+        if (!is_opened(old_line, k) && !is_opened(new_line, k)) continue;
+        if (is_opened(old_line, k) && !is_opened(new_line, k)) return false;
+        if (!is_opened(old_line, k) && is_opened(new_line, k)) return true;
+        if (!is_closed(old_line, k) && !is_closed(new_line, k)) {
+          if (open_time(old_line, k) < open_time(new_line, k)) return false;
+          if (open_time(old_line, k) > open_time(new_line, k)) return true;
+          continue;
+        }
+        if (!is_closed(old_line, k) && is_closed(new_line, k)) return false;
+        if (is_closed(old_line, k) && !is_closed(new_line, k)) return true;
+        if (group_len(old_line, k) > group_len(new_line, k)) return false;
+        if (group_len(old_line, k) < group_len(new_line, k)) return true;
+        if (open_time(old_line, k) > open_time(new_line, k)) return false;
+        if (open_time(old_line, k) < open_time(new_line, k)) return true;
+      }
+      return false;
+    };
 
-  SlotFile* cur_slot_file = &slot_file1;
-  SlotFile* nxt_slot_file = &slot_file2;
-  // use pointer to explicitly eliminate TLS
-  for (auto& line : *cur_slot_file) line.fill(-1);
-  for (auto& line : *nxt_slot_file) line.fill(-1);
-  auto* cur_active_states = &active_states1;
-  auto* nxt_active_states = &active_states2;
-  cur_active_states->clear();
-  cur_active_states->reserve(nr_states);
-  nxt_active_states->clear();
-  nxt_active_states->reserve(nr_states);
-  auto* cur_is_state_active = &is_state_active1;
-  auto* nxt_is_state_active = &is_state_active2;
-  cur_is_state_active->fill(false);
-  nxt_is_state_active->fill(false);
+    thread_local static SlotFile slot_file1, slot_file2;
+    thread_local static std::array<bool, nr_states> is_state_active1, is_state_active2;
+    thread_local static std::vector<size_t> active_states1, active_states2;
 
-  cur_active_states->push_back(0);
-  (*cur_is_state_active)[0] = true;
-  for (size_t idx = 0; idx < str.size(); idx++) {
-    char ch = str[idx];
-    if (ch == '\0') break;
+    SlotFile* cur_slot_file = &slot_file1;
+    SlotFile* nxt_slot_file = &slot_file2;
+    // use pointer to explicitly eliminate TLS
+    for (auto& line : *cur_slot_file) line.fill(-1);
+    for (auto& line : *nxt_slot_file) line.fill(-1);
+    auto* cur_active_states = &active_states1;
+    auto* nxt_active_states = &active_states2;
+    cur_active_states->clear();
+    cur_active_states->reserve(nr_states);
     nxt_active_states->clear();
+    nxt_active_states->reserve(nr_states);
+    auto* cur_is_state_active = &is_state_active1;
+    auto* nxt_is_state_active = &is_state_active2;
+    cur_is_state_active->fill(false);
     nxt_is_state_active->fill(false);
-    for (size_t state : *cur_active_states) {
-      for (const auto& nxt_state : trans_table[state][static_cast<size_t>(ch)]) {
-        if (nxt_state < 0) break;
-        if (!(*nxt_is_state_active)[nxt_state]) {
-          (*nxt_slot_file)[nxt_state] = apply_action(
+
+    cur_active_states->push_back(0);
+    (*cur_is_state_active)[0] = true;
+    for (size_t idx = 0; idx < str.size(); idx++) {
+      char ch = str[idx];
+      if (ch == '\0') break;
+      nxt_active_states->clear();
+      nxt_is_state_active->fill(false);
+      for (size_t state : *cur_active_states) {
+        for (const auto& nxt_state : trans_table[state][static_cast<size_t>(ch)]) {
+          if (nxt_state < 0) break;
+          if (!(*nxt_is_state_active)[nxt_state]) {
+            (*nxt_slot_file)[nxt_state] = apply_action(
+              (*cur_slot_file)[state],
+              trans_action_table[state][static_cast<size_t>(ch)][nxt_state], idx
+            );
+            nxt_active_states->push_back((nxt_state));
+            (*nxt_is_state_active)[nxt_state] = true;
+            continue;
+          }
+          auto next_slot_line = apply_action(
             (*cur_slot_file)[state],
             trans_action_table[state][static_cast<size_t>(ch)][nxt_state], idx
           );
-          nxt_active_states->push_back((nxt_state));
-          (*nxt_is_state_active)[nxt_state] = true;
-          continue;
-        }
-        auto next_slot_line = apply_action(
-          (*cur_slot_file)[state],
-          trans_action_table[state][static_cast<size_t>(ch)][nxt_state], idx
-        );
-        if (need_change((*nxt_slot_file)[nxt_state], next_slot_line)) {
-          (*nxt_slot_file)[nxt_state] = next_slot_line;
+          if (need_change((*nxt_slot_file)[nxt_state], next_slot_line)) {
+            (*nxt_slot_file)[nxt_state] = next_slot_line;
+          }
         }
       }
+      std::swap(cur_slot_file, nxt_slot_file);
+      std::swap(cur_active_states, nxt_active_states);
+      std::swap(cur_is_state_active, nxt_is_state_active);
     }
-    std::swap(cur_slot_file, nxt_slot_file);
-    std::swap(cur_active_states, nxt_active_states);
-    std::swap(cur_is_state_active, nxt_is_state_active);
-  }
 
-  bool is_final_line_inited = false;
-  SlotLine final_line {};
-  for (size_t state : *cur_active_states) {
-    if (!accept_table[state]) continue;
-    if (!is_final_line_inited) {
-      final_line = apply_action(
+    bool is_final_line_inited = false;
+    SlotLine final_line {};
+    for (size_t state : *cur_active_states) {
+      if (!accept_table[state]) continue;
+      if (!is_final_line_inited) {
+        final_line = apply_action(
+          (*cur_slot_file)[state],
+          accept_action_table[state],
+          str.size()
+        );
+        is_final_line_inited = true;
+        continue;
+      }
+      auto after_accept = apply_action(
         (*cur_slot_file)[state],
         accept_action_table[state],
         str.size()
       );
-      is_final_line_inited = true;
-      continue;
-    }
-    auto after_accept = apply_action(
-      (*cur_slot_file)[state],
-      accept_action_table[state],
-      str.size()
-    );
-    if (need_change(final_line, after_accept)) {
-      final_line = after_accept;
-    }
-  }
-
-  std::string result;
-  result.reserve(str.size() + replace_rule.size());
-
-  static auto find_dollar = [](std::string_view str, size_t start_pos) {
-    return std::find(str.begin() + start_pos, str.end(), '$') - str.begin();
-  };
-
-  for (size_t idx = 0; idx < replace_rule.size(); idx++) {
-    size_t nxt_dollar_idx = find_dollar(replace_rule, idx);
-    result.append(replace_rule.data() + idx, nxt_dollar_idx - idx);
-    idx = nxt_dollar_idx;
-    if (idx >= replace_rule.size()) break;
-    if (++idx >= replace_rule.size()) return "";
-    if (replace_rule[idx] == '$') result.append("$");
-    else if (is_digit(replace_rule[idx])) {
-      size_t group_idx = replace_rule[idx] - '0';
-      while (idx + 1 < replace_rule.size() && is_digit(replace_rule[idx + 1])) {
-        idx++;
-        group_idx = 10 * group_idx + replace_rule[idx] - '0';
+      if (need_change(final_line, after_accept)) {
+        final_line = after_accept;
       }
-      if (group_idx >= nr_capture_group) return "";
-      int32_t l = open_time(final_line, group_idx), r = close_time(final_line, group_idx);
-      if (l < 0 || r < 0) continue;
-      result.append(str.data() + l, r - l);
     }
-    else return "";
-  }
 
-  return result;
+    std::string result;
+    result.reserve(str.size() + replace_rule.size());
+
+    static auto find_dollar = [](std::string_view str, size_t start_pos) {
+      return std::find(str.begin() + start_pos, str.end(), '$') - str.begin();
+    };
+
+    for (size_t idx = 0; idx < replace_rule.size(); idx++) {
+      size_t nxt_dollar_idx = find_dollar(replace_rule, idx);
+      result.append(replace_rule.data() + idx, nxt_dollar_idx - idx);
+      idx = nxt_dollar_idx;
+      if (idx >= replace_rule.size()) break;
+      if (++idx >= replace_rule.size()) return "";
+      if (replace_rule[idx] == '$') result.append("$");
+      else if (is_digit(replace_rule[idx])) {
+        size_t group_idx = replace_rule[idx] - '0';
+        while (idx + 1 < replace_rule.size() && is_digit(replace_rule[idx + 1])) {
+          idx++;
+          group_idx = 10 * group_idx + replace_rule[idx] - '0';
+        }
+        if (group_idx >= nr_capture_group) return "";
+        int32_t l = open_time(final_line, group_idx), r = close_time(final_line, group_idx);
+        if (l < 0 || r < 0) continue;
+        result.append(str.data() + l, r - l);
+      }
+      else return "";
+    }
+
+    return result;
+  }
+};
+
+template<char... Cs>
+constexpr auto build_fx_str() {
+  constexpr char arr[] = { Cs..., '\0' };
+  return impl::FixedString<sizeof...(Cs) + 1>(arr);
 }
+
+#if defined(__clang__)
+  #pragma GCC diagnostic push
+  #pragma GCC diagnostic ignored "-Wgnu-string-literal-operator-template"
+#elif defined(__GNUC__)
+  // gcc do not trigger the warning.
+#else
+  static_assert(false, "This version of library does not support compilers other than clang and gcc. Please use the main branch that supports standard C++.");
+#endif
+
+// GNU extension feature, suppress warning to stop compiler complaining
+
+template<typename CharType, CharType... Cs>
+constexpr auto operator""_match() {
+  static_assert(std::is_same_v<CharType, char>, "only permit char string");
+  return Matcher<build_fx_str<Cs...>()> {};
+}
+
+template<typename CharType, CharType... Cs>
+constexpr auto operator""_replace() {
+  static_assert(std::is_same_v<CharType, char>, "only permit char string");
+  return Replacer<build_fx_str<Cs...>()> {};
+}
+
+#if defined(__clang__)
+  #pragma GCC diagnostic pop
+#endif
 
 } /* namespace onre */
 
--- tests/test_head.h
+++ tests/test_head.h
@@ -26,7 +26,7 @@ inline void replace_all(std::string& str, const std::string& from, const std::st
 template<onre::impl::FixedString pattern>
 void test_match_and_log(std::string_view str, bool expected) {
   auto start = std::chrono::high_resolution_clock::now();
-  bool result = onre::match<pattern>(str);
+  bool result = onre::Matcher<pattern>()(str);
   auto end = std::chrono::high_resolution_clock::now();
   std::string s(str);
   replace_all(s, "\n", "\\n");
@@ -50,7 +50,7 @@ void test_match_and_log(std::string_view str, bool expected) {
 template<onre::impl::FixedString pattern>
 void test_replace_and_log(std::string_view rule, std::string_view str, std::string_view expected) {
   auto start = std::chrono::high_resolution_clock::now();
-  std::string result = onre::replace<pattern>(rule, str);
+  std::string result = onre::Replacer<pattern>()(rule, str);
   auto end = std::chrono::high_resolution_clock::now();
   std::string s(str), expected_s(expected);
   replace_all(s, "\n", "\\n");
