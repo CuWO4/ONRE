c3a512acdb3fcab436a467aee5d64d2cfe48d09b
CuWO4 <wutong.tony@outlook.com>
2026-04-29 18:52:48 +0800

    feat: detect whether in global namespace and refuse to compile when it's not

--- src/head.hxx
+++ src/head.hxx
@@ -13,6 +13,26 @@
 #include <type_traits>
 #include <vector>
 
+[[maybe_unused]] static inline void dummy__check_global__a2c7038f() {
+  constexpr auto count_double_colon = [](const char* str) {
+    unsigned cnt = 0;
+    for (char const* p = str; *p && *(p+1); p++)
+      cnt += *p == ':' && *(p+1) == ':';
+    return cnt;
+  };
+
+  static_assert(
+    #if defined(__clang__) || defined(__GNUC__)
+      count_double_colon(__PRETTY_FUNCTION__) == 0,
+    #elif defined(_MSC_VER)
+      count_double_colon(__FUNCSIG__) == 0,
+    #else
+      true,
+    #endif
+    "header is not placed in global namespace"
+  );
+}
+
 // === snippet end ===
 
 #endif
